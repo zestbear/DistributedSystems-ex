95d7fd938eb8d94e0fd4c8bbe43ed9c6cb4fc895
zestbear <zestbear90@gmail.com>
2024-06-25 18:23:34 +0900

    initial commit

---
+++ src/test/java/distributed/system/distributedSystem/DistributedSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package distributed.system.distributedSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
