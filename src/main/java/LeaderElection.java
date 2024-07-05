c04c4d236bad3af013c4ba5c13f72f3b0de5e4f2
zestbear <zestbear90@gmail.com>
2024-07-05 22:19:39 +0900

    feature: 리더 선출 알고리즘 구현

--- src/main/java/LeaderElection.java
+++ src/main/java/LeaderElection.java
@@ -1,22 +1,47 @@
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class LeaderElection implements Watcher {
 
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     private static final int SESSION_TIMEOUT = 3000;
+    private static final String ELECTION_NAMESPACE = "/election";
     private ZooKeeper zooKeeper;
+    private String currentZnodeName;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         LeaderElection leaderElection = new LeaderElection();
 
         leaderElection.connectToZookeeper();
+        leaderElection.volunteerFroLeadership();
+        leaderElection.electLeader();
         leaderElection.run();
         leaderElection.close();
         System.out.println("Disconnected from Zookeeper, exiting application");
+    }
+
+    public void volunteerFroLeadership() throws InterruptedException, KeeperException {
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        System.out.println("znode name " + znodeFullPath);
+        this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+    }
+
+    public void electLeader() throws InterruptedException, KeeperException { // Znode의 자식 노드들을 반환
+        List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+
+        Collections.sort(children);
+        String smallestChild = children.get(0);
+
+        if (smallestChild.equals(currentZnodeName)) { // 기본적으로 가장 작은 노드가 리더가 되지만, 그 첫 번째 노드의 이름과 현재 노드의 이름을 비교
+            System.out.println("I am the leader");
+            return;
+        }
+        System.out.println("I am not the leader, " + smallestChild + " is the leader");
     }
 
     public void connectToZookeeper() throws IOException {
